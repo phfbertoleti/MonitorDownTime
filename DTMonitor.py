b2e3a25910c03280582550abcd0e2750aa642f17
Pedro Bertoleti <phfbertoleti@users.noreply.github.com>
2017-01-29 23:32:23 -0200

    Update DTMonitor.py

--- DTMonitor.py
+++ DTMonitor.py
@@ -2,12 +2,12 @@ import httplib
 import urllib
 import os
 import time
-import pyping
 
 #variaveis globais
 ContaTwetterDoSeuProvedorDeInternet = "@NEToficial"
 NomeDaSuaCidade = "Sao Bernardo do Campo - SP"
 APIKeyThingSpeak = 'KKKKKKKKKKKKKKKK'   #substitua KKKKKKKKKKKKKKKK pela sua api_key
+WriteAPIKey = 'WWWWWWWWWWWWWWWW'   #substitua WWWWWWWWWWWWWWWW pela sua write api key do seu canal
 
 #Downtime monitor variables
 TimestampInicial_DownTime = 0
@@ -18,7 +18,7 @@ NumeroDowntimesDetectados = 0
 NumeroTentativas = 1
 
 def VerificaERegistraDowntime(res):
-    global EstaOffline
+	global EstaOffline
 	global TimestampInicial_DownTime
 	global DownTimeTotal
 	global ArquivoRelatorioDowntime
@@ -27,28 +27,28 @@ def VerificaERegistraDowntime(res):
 
 	if (res == 0):
 		print " "
-		print "Internet está on!"
+		print "Internet esta on!"
 	else:
 		print " "
-		print "Internet está off..."
+		print "Internet esta off..."
 
-    #Verifica se internet estava on e agora esta off
+        #Verifica se internet estava on e agora esta off
 	if ((res!=0) and (EstaOffline == 0)):
 		TimestampInicial_DownTime = time.time()
 		EstaOffline = 1
-        return
+	        return
 
-    #Verifica se internet estava off e agora esta on
+        #Verifica se internet estava off e agora esta on
 	if ((res == 0) and (EstaOffline == 1)):
 		DownTimeTotal = time.time() - TimestampInicial_DownTime
 		StringDT =  "DownTime detectado: "+str(DownTimeTotal)+" segundos offline\n"
-        print StringDT
+	        print StringDT
 		TxtFile = open(ArquivoRelatorioDowntime,"a")
 		TxtFile.write(StringDT)
 		TxtFile.close()
-        NumeroDowntimesDetectados = NumeroDowntimesDetectados + 1
+        	NumeroDowntimesDetectados = NumeroDowntimesDetectados + 1
 		EstaOffline = 0
-        EnviaTweet(DownTimeTotal)
+	        EnviaTweet(DownTimeTotal)
 		EnviaDownTimeThingSpeak(DownTimeTotal)
 		return
 
@@ -58,7 +58,7 @@ def EnviaTweet(DuracaoDT):
 	global APIKeyThingSpeak
         
         
-    StringParaTwettar = ContaTwetterDoSeuProvedorDeInternet+", foi detectado um downtime de "+str(DuracaoDT)+" segundos. Estou na cidade de "+NomeDaSuaCidade+". #DownTimeDetectado"
+        StringParaTwettar = ContaTwetterDoSeuProvedorDeInternet+", foi detectado um downtime de "+str(DuracaoDT)+" segundos. Estou na cidade de "+NomeDaSuaCidade+". #DownTimeDetectado"
 	params = urllib.urlencode({'api_key': APIKeyThingSpeak, 'status': StringParaTwettar})  
 	conn = httplib.HTTPConnection("api.thingspeak.com:80")
 	conn.request("POST","/apps/thingtweet/1/statuses/update",params)
@@ -68,7 +68,7 @@ def EnviaTweet(DuracaoDT):
 def EnviaTweetDownTimeIniciado():
 	global APIKeyThingSpeak
 
-    StringParaTwettar = "Monitor de DownTime iniciado!"
+    	StringParaTwettar = "Monitor de DownTime iniciado!"
 	params = urllib.urlencode({'api_key': APIKeyThingSpeak, 'status': StringParaTwettar})  
 	conn = httplib.HTTPConnection("api.thingspeak.com:80")
 	conn.request("POST","/apps/thingtweet/1/statuses/update",params)
@@ -76,13 +76,13 @@ def EnviaTweetDownTimeIniciado():
 	conn.close()
 	
 def EnviaDownTimeThingSpeak(DuracaoDT):	  	
-	global APIKeyThingSpeak
+	global WriteAPIKey
 
-	params = urllib.urlencode({'field1': str(DuracaoDT),'key': APIKeyThingSpeak})
-    headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-    conn = httplib.HTTPConnection("api.thingspeak.com:80")
-    conn.request("POST", "/update", params, headers) # Tenta fazer uma requisicao
-    resp = conn.getresponse() # Recebe e imprimi na tela a resposta
+	params = urllib.urlencode({'field1': str(DuracaoDT),'key':WriteAPIKey})
+    	headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+	conn = httplib.HTTPConnection("api.thingspeak.com:80")
+    	conn.request("POST", "/update", params, headers) # Tenta fazer uma requisicao
+	resp = conn.getresponse()
 
 
 #----------------------
@@ -94,18 +94,17 @@ EnviaTweetDownTimeIniciado()
 while True:
 	try:
 		os.system("clear")
-       	print "---------------------------"
-	    print "     DownTime Monitor      "
-      	print "---------------------------"
-        print " "
-	    print "Tentativa #"+str(NumeroTentativas)+" - "+str(NumeroDowntimesDetectados)+" downtime(s) detectados"
-        print " "
+       		print "---------------------------"
+	    	print "     DownTime Monitor      "
+      		print "---------------------------"
+	        print " "
+		print "Tentativa #"+str(NumeroTentativas)+" - "+str(NumeroDowntimesDetectados)+" downtime(s) detectados"
+	        print " "
 
 		PingResult = os.system("ping -c 1 8.8.8.8")
-        VerificaERegistraDowntime(PingResult)
+	        VerificaERegistraDowntime(PingResult)
 		time.sleep(20) 
-      	NumeroTentativas = NumeroTentativas + 1
+      		NumeroTentativas = NumeroTentativas + 1
 	except (KeyboardInterrupt):
 		print "Aplicacao encerrada."
 		exit(1)
-	
